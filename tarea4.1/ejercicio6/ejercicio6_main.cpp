c611b65c9382a373199bf76477a46b21db798d6d
Jose M Barba <zerokullneo@gmail.com>
2021-06-02 03:59:30 +0200

    Tarea 4.1
    -Ejercicio 6 de la tarea 4.1

--- tarea4.1/ejercicio6/ejercicio6_main.cpp
+++ tarea4.1/ejercicio6/ejercicio6_main.cpp
@@ -2,20 +2,8 @@
 
 using namespace std;
 
-struct B
-{
-	B() { std::cout << "Constructor (class␣B)\n"; }
-	virtual ~B() { cout << "Destructor (class␣B)\n"; }
-};
-
-struct D: B
-{
-	D() { cout << "Constructor (class D)\n"; }
-	~D() { cout << "Destructor (class D)\n"; }
-};
-
 int main()
 {
-	B *pb = new D;
+	rectangle *pb = new square;
 	delete pb;
 }
